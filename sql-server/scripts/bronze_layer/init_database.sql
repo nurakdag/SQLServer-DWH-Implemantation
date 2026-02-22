e98c078ae83bbb78f4bdcd6696fcdfd4f33e6303
Nur Akdağ <111306424+nurakdag@users.noreply.github.com>
2026-02-22 18:37:36 +0300

    Update init_database.sql

--- sql-server/scripts/bronze_layer/init_database.sql
+++ sql-server/scripts/bronze_layer/init_database.sql
@@ -1,19 +1,4 @@
-/*
-===============================================================================
-Create Database and Schemas
-===============================================================================
-Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking
-    if it already exists. If the database exists, it is dropped and recreated.
-    Additionally, the script sets up three schemas within the database:
-    'bronze', 'silver', and 'gold'.
 
-WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it
-    exists. All data in the database will be permanently deleted. Ensure you
-    have proper backups before running this script.
-===============================================================================
-*/
 
 USE master;
 GO
